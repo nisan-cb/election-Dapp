1fca38c621986a7dfacec6b40c316be354458baa
Nisan <nnisan858@gmail.com>
2024-04-03 23:04:30 +0300

    web3 tools

---
+++ front/src/services/web3.ts
@@ -0,0 +1,34 @@
+import { Contract, ContractAbi } from "web3";
+
+
+// export const getCurrentBlockNumber = () => {
+//     // return new Promise((resolve, reject) => {
+//     console.log('here')
+//     window?.web3.eth.getBlockNumber((error: Error | null, latestBlockNumber: number) => {
+//         if (error) {
+//             console.error('Error getting latest block number:', error);
+//             console.log(0)
+//             // resolve(0)
+//         } else {
+//             console.log(latestBlockNumber)
+//             // resolve(latestBlockNumber)
+//         }
+//     });
+//     // })
+// }
+
+export const subscribeEvent = async (contract: Contract<ContractAbi>, eventName: string, cb?: (event: any) => void) => {
+    try {
+        if (!contract) return
+        const eventInstance = contract?.events[eventName]({ fromBlock: 0 });
+
+        eventInstance?.on('data', (event: any) => {
+            cb?.(event)
+        });
+
+        eventInstance?.on('error', console.error);
+    } catch (error) {
+
+    }
+
+}
